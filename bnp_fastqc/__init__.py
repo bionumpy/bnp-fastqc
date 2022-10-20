cee6b718b8f7f968e342aca8168f89757ac2e118
Ivar Grytten <ivar.grytten@gmail.com>
2022-10-20 11:22:54 +0200

    Command line testing

--- bnp_fastqc/__init__.py
+++ bnp_fastqc/__init__.py
@@ -1,5 +1,1 @@
 """Top-level package for bnp-fastqc."""
-
-__author__ = """Ivar Grytten"""
-__email__ = 'ivargry@ifi.uio.no'
-__version__ = '0.1.0'
--- bnp_fastqc/cli.py
+++ bnp_fastqc/cli.py
@@ -1,12 +1,21 @@
 """Console script for bnp_fastqc."""
 import sys
 import click
+import modules
+
+
+MODULES = {
+   "kmer_content": modules.kmer_content
+}
 
 
 @click.command()
 @click.option("--reads")
-def main(reads):
-    print(reads)
+@click.option("--module")
+def main(reads, module):
+    if module not in MODULES:
+        print("Invalid module", module, ". Valid modules are: %s" % ', '.join(MODULES.keys()))
+        return
 
     return 0
 
